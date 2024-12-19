00bda757e5ffce0d93c7f22608a6e92c3cbca4d6
7rizz1 <riizwanalam@gmail.com>
2024-12-19 14:40:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log(3+3);
+
+let name = "rizwan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.table([name, age, isLoggedIn])
+
+console.log(typeof isLoggedIn)
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Rizwan")
+const accountId = 1345433
+let accountEmail = "rizwan@gmail.com"
+var accountPassword = "3211"
+accountCity = "Delhi"
+let accountState;
+
+accountEmail ="hue@gmail.com"
+accountPassword = "011"
+accountCity = "hati"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountState, accountPassword, accountCity])
