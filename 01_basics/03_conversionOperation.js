3498b6b882ae1148ef6de5dfc50567750589eca7
7rizz1 <riizwanalam@gmail.com>
2024-12-19 15:02:47 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanisloggedin = Boolean(isLoggedIn)
+
+console.log(booleanisloggedin);
+
+let someNumber =23
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
